6b184b786a0c114dd4274d98071fab943c2d9030
MariamAtef226 <68165688+MariamAtef226@users.noreply.github.com>
2023-07-19 18:41:15 +0300

    Add files via upload

--- gui.py
+++ gui.py
@@ -6,27 +6,29 @@ import tkinter as tk
 def get_urls(url, n):
     urls = []
     try:
-        response = requests.get(url)
-        html = bs4.BeautifulSoup(response.content, 'html.parser')
-        all_links = html.find_all('a', href=True)
+        response = requests.get(url) # start request
+        html = bs4.BeautifulSoup(response.content, 'html.parser') # get html content
+        all_links = html.find_all('a', href=True) # extract the anchor tags only
         for link in all_links:
-            url = link['href']
-            if ('http' in url or 'www' in url) and url not in urls:
+            url = link['href'] # extract the href attribute value
+            if ('http' in url or 'www' in url) and url not in urls: # extract valid links and unique one for this depth
                 urls.append(url)
 
-        if n > 1:
+        if n > 1: # base case
             temp = []
             for url in urls:
-                more_urls = get_urls(url, n - 1)
+                more_urls = get_urls(url, n - 1) # dive into next depth
                 temp.extend(more_urls)
-            urls.extend(temp)
+            urls.extend(temp) # used extend not append to merge the 2 lists
     except:
-        print("Failed to Access Page")
+        print("Failed to Access Page") # handle exceptions due to failing to access some links
+        # due to connection timeout or unauthorised access
     finally:
         return urls
 
 
 def main():
+    # gui
     root = tk.Tk()
     root.title("URLs Scrapper")
 
@@ -58,8 +60,8 @@ def search(url, n):
 
     for url in urls:
         urls_list.insert(tk.END, url + '\n')
-    result.mainloop()
 
+    result.mainloop()
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -4,22 +4,23 @@ import bs4
 def get_urls(url, n):
     urls = []
     try:
-        response = requests.get(url)
-        html = bs4.BeautifulSoup(response.content, 'html.parser')
-        all_links = html.find_all('a', href=True)
+        response = requests.get(url) # start connection
+        html = bs4.BeautifulSoup(response.content, 'html.parser')  # get html content
+        all_links = html.find_all('a', href=True) # extract the anchor tags only
         for link in all_links:
-            url = link['href']
-            if ('http' in url or 'www' in url) and url not in urls:
+            url = link['href'] # extract the href attribute value
+            if ('http' in url or 'www' in url) and url not in urls: # extract valid links and unique one for this depth
                 urls.append(url)
 
         if n > 1:
             temp = []
             for url in urls:
-                more_urls = get_urls(url, n - 1)
+                more_urls = get_urls(url, n - 1)  # dive into next depth
                 temp.extend(more_urls)
-            urls.extend(temp)
+            urls.extend(temp) # used extend not append to merge the 2 lists
     except:
-        print("Failed to Access Page")
+        print("Failed to Access Page") # handle exceptions due to failing to access some links
+        # due to connection timeout or unauthorised access
     finally:
         return urls
 
@@ -27,8 +28,6 @@ def get_urls(url, n):
 def main():
     url = input('Enter the URL: ')
     n = int(input('Enter search depth: '))
-    print(url)
-    print(n)
     urls = get_urls(url, n)
     print(urls)
 
